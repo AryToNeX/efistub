fc471dcf101b91be44283ad63e0b42ae5dfd447e
Naomi Calabretta <me@arytonex.pw>
2021-05-22 17:01:44 +0200

    Initial commit

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/sh 
+
+curl -o /usr/bin/efistub "https://raw.githubusercontent.com/AryToNeX/efistub/master/efistub"
+curl -o /etc/default/efistub "https://raw.githubusercontent.com/AryToNeX/efistub/master/efistub.default"
+
+chmod +x /usr/bin/efistub
+
+echo "All done! Please remember to edit the file at /etc/default/efistub before using the utility"
